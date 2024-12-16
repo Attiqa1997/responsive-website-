b824ad632f9aa690ea70e5108cec4ee982cc397c
Attiqa1997 <Attiqaaslam1997@gmail.com>
2024-12-17 00:03:06 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Navbar from "./components/navbar";
+import Middle from "./components/middle navbar";
+export default function Home() {
+  return (
+<div>
+  <Navbar/>
+  <Middle/>
+<main></main>
+</div>
+
+  )
+}
